fd0bda5a1edbcbef07537724832023f9b0b8eab6
TebohoRomeo <teboholekhalo13@gmail.com>
2020-01-15 15:12:21 +0200

    Updates

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,29 +1,74 @@
-let person = require("../src/person")
+let person = require('../src/person');
 
-describe("Person class", function(){
+describe('Person class', function() {
+// Test for Romeo
+  it('Records the age', function() {
+    const romeo = new person('Romeo', 30, 'male', [
+      'being a hardarse',
+      'agile',
+      'ssd hard drives',
+    ]);
 
-    it("Records the age", function(){
-        const romeo = new person("Romeo",30,"male",["being a hardarse","agile", "ssd hard drives"])
+    expect(romeo.age).toEqual(30);
+  });
 
-        expect(romeo.age).toEqual(30);
-    })
+  it('checks if hello() contains a message', function() {
+    const romeo = new person('Romeo', 22, 'male', [
+      'being a hardarse',
+      'agile',
+      'ssd hard drives',
+    ]);
 
-    it("checks if hello() contains a message", function(){
-        const romeo = new person("Romeo",22,"male",["being a hardarse","agile", "ssd hard drives"])
+    expect(romeo.hello()).toEqual(
+      'Hello, my name is Romeo and I am 22 Years old male. My interests are being a hardarse, agile and ssd hard drives.'
+    );
+  });
 
-        expect(romeo.hello()).toEqual("Hello, my name is Romeo and I am 22 Years old male. My interests are being a hardarse, agile and ssd hard drives.");
-    })
+//   Test for Mudi
 
-    it("checks if hello() contains a message", function(){
-        const mudi = new person("Mudi",30,"male",["being a hardarse","nerdy", "ssd hard drives"])
+  it('checks if hello() contains a message', function() {
+    const mudi = new person('Mudi', 30, 'male', [
+      'being a hardarse',
+      'nerdy',
+      'ssd hard drives',
+    ]);
 
-        expect(mudi.hello()).toEqual("Hello, my name is Mudi and I am 30 Years old male. My interests are being a hardarse, nerdy and ssd hard drives.");
-    })
+    expect(mudi.hello()).toEqual(
+      'Hello, my name is Mudi and I am 30 Years old male. My interests are being a hardarse, nerdy and ssd hard drives.'
+    );
+  });
 
-    it("Records the age", function(){
-        const mudi = new person("Mudi",30,"male",["being a hardarse","agile", "ssd hard drives"])
+  it('Records the age', function() {
+    const mudi = new person('Mudi', 30, 'male', [
+      'being a hardarse',
+      'agile',
+      'ssd hard drives',
+    ]);
 
-        expect(mudi.age).toEqual(30);
-    })
+    expect(mudi.age).toEqual(30);
+  });
 
-})
+//   Test for Ryan
+
+it('Records the age', function() {
+    const ryan = new person('Ryan', 30, 'male', [
+      'being a hardarse',
+      'agile',
+      'ssd hard drives',
+    ]);
+
+    expect(ryan.age).toEqual(30);
+  });
+
+  it('checks if hello() contains a message', function() {
+    const ryan = new person('Ryan', 30, 'male', [
+      'being a hardarse',
+      'agile',
+      'ssd hard drives',
+    ]);
+
+    expect(ryan.hello()).toEqual(
+      'Hello, my name is Ryan and I am 30 Years old male. My interests are being a hardarse, agile and ssd hard drives.'
+    );
+  });
+});
--- src/person.js
+++ src/person.js
@@ -1,37 +1,51 @@
 // Author: Teboho Romeo Lekhalo
 // Date: 13 January 2020.
-'use strict'
+'use strict';
 
 class Person {
+  constructor(name, age, gender, interest) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interest = interest;
+  }
 
-    constructor(name, age,gender, interest){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.interest = interest;
-    }
+  hello() {
+    let greet =
+      'Hello, my name is' +
+      ' ' +
+      this.name +
+      ' ' +
+      'and I am' +
+      ' ' +
+      this.age +
+      ' ' +
+      'Years old' +
+      ' ' +
+      this.gender +
+      '.' +
+      ' My interests are '
 
-    hello(){
-        let greet = "Hello, my name is" + " " + this.name + " " + "and I am" + " " + this.age + " " + "Years old" + " " + this.gender + "." + " My interests are ";
-
-        for (let i = 0; i < this.interest.length; i++){
-            if (i == this.interest.length - 2){
-                greet += this.interest[i] + " and ";
-            }
-            else if (i == this.interest.length - 1){
-                greet += this.interest[i] + ".";
-            }
-            else {
-                greet += this.interest[i] + ", ";
-            }
+    for (let i = 0; i < this.interest.length; i++){
+        if (i == this.interest.length - 2){
+            greet += this.interest[i] + " and ";
         }
-        return greet;
+        else if (i == this.interest.length - 1){
+            greet += this.interest[i] + ".";
+        }
+        else {
+            greet += this.interest[i] + ", ";
+        }
     }
-    
+    return greet;
+  }
 }
 
-
-let person = new Person('Ryan', 30,'male',['being a hardarse', ' agile', ' ssd hard drives']);
+let person = new Person('Ryan', 30, 'male', [
+  'being a hardarse',
+  'agile',
+  'ssd hard drives'
+]);
 
 let greeting = person.hello();
 // console.log(greeting);
